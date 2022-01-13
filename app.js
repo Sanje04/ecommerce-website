904b5d5926ba3b9dc575d623da0a383a2d055ef8
Sanje Divakaran <software@aheadsimulations.com>
2022-01-12 20:37:52 -0500

    User signup

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-Parser')
 require('dotenv').config();
 // import routes
 const userRoutes = require('./routes/user')
@@ -11,6 +14,11 @@ const app = express()
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
 }).then(() => console.log('DB Connected'))
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // routes middleware
 app.use('/api', userRoutes)
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,17 @@
-exports.sayHi = (req, res) => {
-    res.json({
-        message: "hello there"
+const User = require('../models/user')
+
+exports.signup = (req, res) => {
+    console.log("req.body", req.body);
+    const user = new User(req.body);
+
+    user.save((error) => {
+        if(err) {
+            return res.status(400).json({
+                error
+            })
+        }
+        res.json({
+            user
+        });
     })
 }
