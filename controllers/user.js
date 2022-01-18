d6f46e2f87c7738574214f4ee918c65b5c911665
Sanje Divakaran <software@aheadsimulations.com>
2022-01-18 04:21:28 -0500

    database updates

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const {errorHandler} = require('../helpers/dbErrorHandler')
 
 exports.signup = (req, res) => {
     //console.log("req.body", req.body);
@@ -7,9 +8,11 @@ exports.signup = (req, res) => {
     user.save((error, user) => {
         if(error) {
             return res.status(400).json({
-                error
+                error: errorHandler(error)
             })
         }   
+        user.salt = undefined;
+        user.hashed_password = undefined;
         res.json({
             user
         });
