8b3872583ab6ba3919682c6824809c6112674119
Sanje Divakaran <software@aheadsimulations.com>
2022-01-20 02:43:08 -0500

    creating product

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
 
 // app
 const app = express()
@@ -29,6 +30,7 @@ app.use(expressValidator());
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
 app.use('/api', categoryRoutes)
+app.use('/api', productRoutes)
 
 
 
--- models/product.js
+++ models/product.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { ObjectID } = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
 
 const productSchema = new mongoose.Schema(
     {
@@ -21,7 +21,7 @@ const productSchema = new mongoose.Schema(
             maxlength: 32
         },
         category: {
-            type: ObjectID,
+            type: ObjectId,
             ref: 'Category',
             required: true
         },
