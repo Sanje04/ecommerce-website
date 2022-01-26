a8496cb0292d7c67a6e31ecc806582cd73fa24bb
Sanje Divakaran <software@aheadsimulations.com>
2022-01-26 00:37:16 -0500

    category updates/delete/list

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router()
 
-const { create, categoryById, read } = require('../controllers/category');
+const { create, categoryById, read, update, remove, list } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.get('/category/:categoryId', read)
 //route method
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+//updating
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+//deleting
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+//getting all the category
+router.get('/categories', list)
+
 
 
 router.param('categoryId', categoryById)
