6cca9cefd8964281d8b335198e21f0154c9b2077
Sanje Divakaran <software@aheadsimulations.com>
2022-01-26 00:05:34 -0500

    product update

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,6 @@
-const Category = require('../models/product');
 const formidable = require('formidable');
 const _ = require("lodash");
-const product = require('../models/product');
+const { product, Category } = require('../models/product');
 const fs = require('fs')
 const {errorHandler} = require('../helpers/dbErrorHandler')
 
@@ -82,6 +81,56 @@ exports.remove = (req, res) => {
         }
         res.json({
             'message': "product has been deleted"
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        // check for all fields
+        const { name, desc, price, category, quantity, shipping } = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            //show error message
+            return res.status(400).json({
+                error: 'all fields are required'
+            })
+        }
+        //no error
+        let product = req.product
+        product = _.extend(product, fields)
+
+        // 1kb = 1000
+        // 1mb = 1000000
+
+        if(files.photo) {
+            //photo from client size 
+            //console.log('FILES PHOTO', files.photo)
+            //checking the size of the file, the image should be es
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'image should be less than 1mb in size'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
         })
     })
 }
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 
-const { create, productById, read, remove } = require('../controllers/product');
+const { create, productById, read, remove, update } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -10,6 +10,9 @@ router.get('/product/:productId', read)
 
 //deleting a product from db
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove)
+
+//updating a product
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update)
 
 //route method
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
